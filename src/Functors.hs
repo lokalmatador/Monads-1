3cba422de7baaf102ba3e88fa0ac14247b76fa74
Philipp Zech <zech.ph@gmail.com>
2021-01-23 18:42:38 +0100

    fix todos

--- src/Functors.hs
+++ src/Functors.hs
@@ -53,7 +53,8 @@ instance Functor (Either a) where
 
 -- TODO: This function needs a type signature!
 --       Make it as general as possible!
--- convertTupleFunctor = fmap personFromTuple
+convertTupleFunctor :: Functor f => f (String, String, Int) -> f Person
+convertTupleFunctor = fmap personFromTuple
 
 -- Making our own Functor
 
@@ -65,8 +66,12 @@ data GovDirectory a = GovDirectory {
 }
 
 instance Functor GovDirectory where
-  -- TODO: Write out this functor instance!
-  fmap f oldDirectory = undefined
+  fmap f oldDirectory = GovDirectory {
+    mayor = f (mayor oldDirectory),
+    interimMayor = fmap f (interimMayor oldDirectory),
+    cabinet = fmap f (cabinet oldDirectory),
+    councilMembers = f <$> councilMembers oldDirectory
+  }
 
 oldDirectory :: GovDirectory (String, String, Int)
 oldDirectory = GovDirectory
@@ -82,4 +87,4 @@ oldDirectory = GovDirectory
 -- TODO: How can we do this in general terms, since we have
 --       a Functor instance?
 newDirectory :: GovDirectory Person
-newDirectory = undefined
+newDirectory = personFromTuple <$> oldDirectory
